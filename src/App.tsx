0843d9b158eaa70532b4161f8e3f3e721a0a9b4b
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-04 16:04:15 +0000

    cleaned up app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-// import { Environment, OrbitControls } from '@react-three/drei'
-// import { Canvas, useLoader } from '@react-three/fiber'
-// import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
-
 import { useState } from 'react'
 import Hero from './components/Hero'
-// import { Suspense } from 'react'
-// import { TextureLoader } from 'three'
-
-// const Model = () => {
-//   const obj = useLoader(OBJLoader, './love_heart.obj')
-//   return <primitive object={obj} />
-// }
 
 function App() {
-  // const colorMap = useLoader(TextureLoader, './red_texture.jpg')
   const [isClicked, setClick] = useState(false)
 
   const handleClick = () => {
@@ -22,17 +10,16 @@ function App() {
 
   return (
     <>
+      {/* bg-gradient-to-r from-pink-600 via-pink-700 to-rose-800  */}
       <div
-        className={` box ${
-          isClicked ? 'bg-petals' : ''
-        }  bg-gradient-to-r from-pink-600 via-pink-700 to-rose-800 w-screen h-screen `}
+        className={`bg-gradient-to-r from-pink-500 via-pink-700 to-rose-800 ${
+          isClicked ? 'bg-hero-pattern' : ''
+        }  w-screen h-screen`}
       >
-        <div className="overflow-hidden flex w-screen h-screen justify-center items-center z-50">
-          <Hero
-            onClickHandler={handleClick}
-            isClicked={isClicked}
-          />
-        </div>
+        <Hero
+          onClickHandler={handleClick}
+          isClicked={isClicked}
+        />
       </div>
     </>
   )

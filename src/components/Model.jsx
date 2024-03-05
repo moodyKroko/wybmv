5bf9ff3ae352edea3cdd238bf4e7d3866a126298
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-05 17:17:11 +0000

    update: remove position attributes
    update: remove redundant refs

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,22 +1,19 @@
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Model({ ...props }) {
   const group = useRef()
-  const posRef = useRef()
   const { nodes, materials } = useGLTF('/model.glb')
 
   return (
     <group ref={group} {...props} dispose={null}>
       <group rotation={[-Math.PI / 2, 0, 0]}>
         <group>
-          {/* // position={[0, 0, 33.415]} */}
           <mesh
-            ref={posRef}
+            castShadow={true}
             geometry={nodes.Object_3.geometry}
             material={materials.Material_30}
-            // position={[0, 0, -33.415]}
           />
         </group>
       </group>

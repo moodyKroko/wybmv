65bc1651b2bccb773e189b53b1bf4c3c3f09c5a4
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-06 02:15:34 +0000

    feat: add initial spin on the heart model
    fast spin of heart model when first enter or refresh then
    gradually slow it down and keep it rotation at steady pace after
    
    feat: add spin to model when first loaded
    
    Add gradually slowing spin to the model when first loaded.

--- src/components/HeartCanvas.tsx
+++ src/components/HeartCanvas.tsx
@@ -1,29 +1,39 @@
-import { Suspense } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 
 import { OrbitControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 
 import Model from './Model.jsx'
+import Spinner from './Spinner.tsx'
 
 export default function HeartCanvas() {
+  const [speed, setSpeed] = useState(100)
+
+  // gradually slowdown the rotation of model after loading
+  const rotateSpeeds = [305, 505, 305, 155, 55, 5]
+  const delayTimes = [1, 2, 3, 4, 5, 6]
+  useEffect(() => {
+
+    for (let i = rotateSpeeds.length - 1; i >= 0; i--) {
+      setTimeout(() => {
+        setSpeed(rotateSpeeds[i])
+      }, 110 * delayTimes[i])
+    }
+  }, [])
+
   return (
     <Canvas
       camera={{ position: [0, 60, 200], fov: 60 }}
-      style={{
-        backgroundColor: '#111a21',
-        width: '100vw',
-        height: '100vh',
-      }}
     >
-      <ambientLight intensity={1.25} />
+      <ambientLight color={0x404040} intensity={1.25} />
       <ambientLight intensity={0.5} />
-      <directionalLight intensity={2.6} castShadow={true} />
-      <Suspense fallback={null}>
+      <directionalLight intensity={3.6} castShadow={true} />
+      <Suspense fallback={<Spinner />}>
         <Model position={[0.025, -0.9, 0]} />
       </Suspense>
       <OrbitControls
         autoRotate={true}
-        autoRotateSpeed={3}
+        autoRotateSpeed={speed}
         enableZoom={false}
         minPolarAngle={1.3}
         maxPolarAngle={0}

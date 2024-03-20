305500ce331b84cfba834b22f50150ac345496f2
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-20 16:40:00 +0000

    updated loading letter and added new buttons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,18 @@
-import { EnvelopeIcon } from '@heroicons/react/24/solid'
 import { AnimatePresence, motion } from 'framer-motion'
 import { MouseEventHandler } from 'react'
 import HeartModel from './HeartModel'
 
+import Letter from '../assets/letter.png'
+
 const Hero = (params: {
-  onClickHandler: MouseEventHandler<SVGSVGElement> | undefined
+  onClickHandler: MouseEventHandler<HTMLImageElement>
   isClicked: boolean
+  isYesClicked: MouseEventHandler<HTMLButtonElement>
 }) => {
   return (
-    <div className="flex justify-center items-center h-3/4 ">
+    <div
+      className={` sm:container sm:mx-auto md:container md:mx-auto w-screen h-screen flex justify-center items-center bg-center relative`}
+    >
       <AnimatePresence mode="popLayout">
         {
           // if
@@ -21,13 +25,13 @@ const Hero = (params: {
               <motion.div
                 className="box"
                 animate={{
-                  rotate: [0, 5, 0, -5, 0, 5, 0, -5, 0] // [0, 0, 180, 180, 0],
+                  rotate: [0, 5, 0, -5, 0, 5, 0, -5, 0], // [0, 0, 180, 180, 0],
                 }}
                 transition={{
                   duration: 0.3,
                   ease: 'linear',
                   repeat: Infinity,
-                  repeatDelay: 1.5
+                  repeatDelay: 1.5,
                 }}
               >
                 <motion.div
@@ -36,8 +40,9 @@ const Hero = (params: {
                   whileTap={{ scale: 0.9 }}
                   transition={{ type: 'spring', stiffness: 400, damping: 17 }}
                 >
-                  <EnvelopeIcon
-                    className=" w-[34rem] h-5/6 text-pink-600 cursor-rose"
+                  <img
+                    src={Letter}
+                    className="w-96 h-auto cursor-rose"
                     onClick={params.onClickHandler}
                   />
                 </motion.div>
@@ -46,18 +51,41 @@ const Hero = (params: {
           ) : (
             // or
             <motion.div
-              className="h-3/4"
               key="openEnvelope"
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
+              className="relative"
             >
-              <HeartModel />
-              <div className="absolute left-0 right-0 bottom-1/4 text-center z-10 mb-10">
-                <h1 className="font-display text-8xl capitalize text-white ">
+              <div className="fixed w-fit h-fit pt-9 top-0 left-0 right-0 -z-0">
+                <HeartModel />
+              </div>
+
+              <div className="pt-20">
+                <h1 className="text-center font-display text-8xl text-wrap capitalize leading-snug text-pink-800 p-8 mt-9 -z-20">
                   will you be my valentine!!
                 </h1>
+
+                <div className="flex justify-around">
+                  <motion.button
+                    className="min-w-32 p-2 rounded-md  bg-red-800 text-center text-white hover:bg-rose-700 shadow-md"
+                    whileTap={{ scale: 0.9 }}
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+                    onClick={params.isYesClicked}
+                  >
+                    Yes
+                  </motion.button>
+                  <motion.a
+                    href="/"
+                    className="min-w-32 p-2 rounded-md  bg-red-800 text-center text-white hover:bg-rose-700 shadow-md z-50"
+                    whileTap={{ scale: 0.9 }}
+                    whileHover={{ scale: 1.1 }}
+                    transition={{ type: 'spring', stiffness: 400, damping: 17 }}
+                  >
+                    Back Home
+                  </motion.a>
+                </div>
               </div>
-              {/* <EnvelopeOpenIcon className=" w-[34rem] h-5/6 text-pink-600 cursor-rose" /> */}
             </motion.div>
           )
         }

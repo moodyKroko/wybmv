667bbcabf1575050143d755b23f41f1cd919d81e
Russel <59341541+moodyKroko@users.noreply.github.com>
2024-03-20 16:40:40 +0000

    removed gradient and petal background

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
 import Hero from './components/Hero'
+import { useState } from 'react'
 
 function App() {
   const [isClicked, setClick] = useState(false)
+  const [yes, setYes] = useState(false)
+
+  const handleYesClick = () => {
+    setYes(true)
+  }
 
   const handleClick = () => {
     setClick(true)
@@ -10,16 +15,20 @@ function App() {
 
   return (
     <>
-      {/* bg-gradient-to-r from-pink-600 via-pink-700 to-rose-800  */}
-      <div
-        className={`bg-gradient-to-r from-pink-500 via-pink-700 to-rose-800 ${
-          isClicked ? 'bg-hero-pattern' : ''
-        }  w-screen h-screen`}
-      >
-        <Hero
-          onClickHandler={handleClick}
-          isClicked={isClicked}
-        />
+      <div className="bg-hero-pattern bg-hero-color w-screen h-screen">
+        <div
+          className={`${
+            yes
+              ? 'bg-[url(https://media.tenor.com/FV8Tn_aeadUAAAAi/petals.gif)]'
+              : ''
+          }`}
+        >
+          <Hero
+            onClickHandler={handleClick}
+            isClicked={isClicked}
+            isYesClicked={handleYesClick}
+          />
+        </div>
       </div>
     </>
   )
